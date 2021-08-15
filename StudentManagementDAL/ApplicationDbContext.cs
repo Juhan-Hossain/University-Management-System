424f4bf68690a47a043dd9fc71bd85b732263dfa
Juhan Hossain <Juhanhossain@gmail.com>
2021-08-16 05:23:02 +0600

    .gitignore is now working

--- StudentManagementDAL/ApplicationDbContext.cs
+++ StudentManagementDAL/ApplicationDbContext.cs
@@ -8,8 +8,34 @@ using System.Threading.Tasks;
 
 namespace StudentManagementDAL
 {
-    public class ApplicationDbContext: DbContext
+    public class ApplicationDbContext : DbContext
     {
-       
+        public DbSet<Department> Departments { get; set; }
+        public DbSet<Student> Students { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer("Data Source=BS-161\\SQLEXPRESS;Initial Catalog=StudentManagementSystem;Integrated Security=True");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Student>(entity =>
+            {
+
+                entity.HasIndex(x => x.Email).IsUnique();
+                entity.HasIndex(x => x.ContactNumber).IsUnique();
+                entity.HasIndex(x => x.RegistrationNumber).IsUnique();
+            });
+
+            modelBuilder.Entity<Department>(entity =>
+            {
+                entity.HasIndex(x => x.Name).IsUnique();
+                entity.HasIndex(x => x.Code).IsUnique();
+            });
+        }
     }
 }
