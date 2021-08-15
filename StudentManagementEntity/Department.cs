87f966eaf46eeb035cbaa376a2291364ca85fd44
Juhan Hossain <Juhanhossain@gmail.com>
2021-08-16 05:04:19 +0600

    Remove the now ignored directory .vs

---
+++ StudentManagementEntity/Department.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudentManagementEntity
+{
+    public class Department
+    {
+        public Department()
+        {
+            Students = new HashSet<Student>();
+        }
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        public string Code { get; set; }
+        public ICollection<Student> Students { get; set; }
+    }
+}
---
+++ StudentManagementEntity/Student.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudentManagementEntity
+{
+    public class Student
+    {
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        [DataType(DataType.EmailAddress)]
+        [EmailAddress(ErrorMessage = "Please Enter Email Address in correct Format.")]
+        public string Email { get; set; }
+        public DateTime Date { get; set; }
+        public long ContactNumber { get; set; }
+        [Required]
+        public string Address { get; set; }
+
+        public Department? Department { get; set; }
+
+        public int DepartmentId { get; set; }
+
+        [Required]
+        public string RegistrationNumber { get; set; }
+    }
+}
