92340e13f37646f3a60dbe6d44e8345c8a0d1492
Juhan Hossain <Juhanhossain@gmail.com>
2021-08-17 16:30:18 +0600

    Added Teacher Functionality

--- StudentManagementDAL/Migrations/20210817102818_fixing_teacher_table.Designer.cs
+++ StudentManagementDAL/Migrations/20210817102818_fixing_teacher_table.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using StudentManagementDAL;
 
 namespace StudentManagementDAL.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20210817102818_fixing_teacher_table")]
+    partial class fixing_teacher_table
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
