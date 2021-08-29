ad9d6654f447234b52a527b090ee4ce6e98a4de3
Juhan Hossain <Juhanhossain@gmail.com>
2021-08-29 17:23:31 +0600

    added getGrades  endpoint for feature11

---
+++ University Student Management/Controllers/GradesController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using RepositoryLayer;
+using StudentManagementBLL.GradeBLL;
+using StudentManagementEntity;
+
+namespace University_Student_Management.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class GradesController:ControllerBase
+    {
+        private readonly IGradeBLL _service;
+        public GradesController(IGradeBLL service)
+        {
+            _service = service;
+        }
+
+        // GET: api/Grades:All
+        [HttpGet("GetGrades")]
+        public ActionResult<ServiceResponse<IEnumerable<StudentGrade>>> GetGrades()
+        {
+            var serviceResponse = _service.GetAll();
+            if (serviceResponse.Success == false) return BadRequest(serviceResponse.Message);
+            return Ok(serviceResponse);
+        }
+    }
+}
